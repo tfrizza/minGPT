1bfcf407481f3dadd56d6c3344e079e21cf92e56
tfrizza <tfrizza@atlassian.com>
2020-10-18 15:22:35 +1100

    Manual GeLU activation

--- mingpt_tf/model.py
+++ mingpt_tf/model.py
@@ -56,6 +56,21 @@ class GPT1Config(GPTConfig):
     n_layer = 12
     n_head = 12
     n_embd = 768
+    
+def gelu(x):
+    """Gaussian Error Linear Unit.
+    This is a smoother version of the RELU.
+    Original paper: https://arxiv.org/abs/1606.08415
+    Args:
+        x: float Tensor to perform activation.
+    Returns:
+        `x` with the GELU activation applied.
+    """
+    cdf = 0.5 * (1.0 + tf.tanh(
+        (math.sqrt(2 / math.pi) * (x + 0.044715 * tf.pow(x, 3)))))
+    return x * cdf
+
+GeLU = tf.keras.activations.get(gelu)
 
 class CausalSelfAttention(K.layers.Layer):
     """
@@ -147,7 +162,7 @@ class Block(K.layers.Layer):
         self.ln2 = LayerNormalization()
         self.attn = CausalSelfAttention(config)
         self.mlp = K.Sequential([
-            Dense(4 * config.n_embd, activation='gelu', kernel_initializer='he_normal', bias_initializer='zeros'),
+            Dense(4 * config.n_embd, activation=GeLU, kernel_initializer='he_normal', bias_initializer='zeros'),
             Dense(config.n_embd, kernel_initializer='he_normal', bias_initializer='zeros'),
             Dropout(config.resid_pdrop),
         ])
